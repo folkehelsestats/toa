6a6f55482fe69e24c662a54d6dbba2bad615ba33
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-07-21 14:13:55 +0200

    Add tcltk package

--- rus/setup.R
+++ rus/setup.R
@@ -1,7 +1,7 @@
 
 # Description: This script reads in the data from the Rusundersøkelsen 2024 dataset.
 # The codes here is based on Stata file "til tall om alkohol.do"
-pkgs <- c("data.table", "haven", "skimr", "codebook")
+pkgs <- c("data.table", "haven", "skimr", "codebook", "tcltk")
 invisible(lapply(pkgs, function(pkg) {
     if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
     library(pkg, character.only = TRUE)
