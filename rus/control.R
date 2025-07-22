31384da55bed927103b75f0c460cf268b9120ca4
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-07-22 15:41:19 +0200

    Check for double registration

--- rus/control.R
+++ rus/control.R
@@ -15,3 +15,42 @@ vars <- c("Type1b_1", "Type1b_2",
           "Type4c_1", "Type4c_2")
 
 outtyp <- lapply(vars, function(v) dt[, .N, keyby = v])
+
+## -- Alkoholfrekvens
+
+ttbl("Drukket1")
+ttbl("Drukket2")
+ttbl("Drukk1b")
+
+dt[Drukket1 == 2 & Drukk1b == 1, .N] #ikke drukket siste år
+ttbl("alkofrekvens")
+
+## Calculate alcohol units assuming 1.5 unit for 500ml beer
+## Replace missing with 0
+dt[, olenheter2 :=
+      (fcoalesce(Type1b_1, 0) + 1.5 * fcoalesce(Type1b_2, 0)) * 4 +
+      fcoalesce(Type1c_1, 0) + 1.5 * fcoalesce(Type1c_2, 0)
+]
+
+dt[, .(olenheter, olenheter2, flaskeroluke, halvliteroluke, Type1b_1, Type1b_2, Type1c_1, Type1c_2)]
+
+## No multiple answer for alcohol units
+cols <- c("Type1b_1", "Type1b_2", "Type1c_1", "Type1c_2")
+olcol <- c("flaskeroluke", "halvliteroluke", "flaskeroltot", "halvlitertot" )
+dt[, olmulti4 := rowSums(!is.na(.SD)) == length(cols), .SDcols = cols]
+dt[, .N, keyby = olmulti4]
+
+dt[, olmulti2a := rowSums(!is.na(.SD)) == length(cols[1:2]), .SDcols = cols[1:2]]
+dt[, .N, keyby = olmulti2a]
+cols2a <- c("olmulti2a", "olenheter2", "olenheter", cols)
+dt[olmulti2a == 1, ..cols2a]
+
+## check average drinking amount weekly and monthly
+OlVars <- c("Type1b_1", "Type1b_2", "Type1c_1", "Type1c_2")
+dt[, lapply(.SD, function(x)
+  c(
+    min(x, na.rm = TRUE),
+    max(x, na.rm = TRUE),
+    median(x, na.rm = T),
+    tail(sort(x))
+  )),.SDcols = OlVars ]
