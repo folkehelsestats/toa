b208e3e86e262f7472ad95f59ffbfe8cf5b43b61
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-07-25 12:42:19 +0200

    Create an RDS file

--- rus/setup.R
+++ rus/setup.R
@@ -4,7 +4,7 @@
 # ----------------------------------
 # tcltk - to display the CRAN windows when not active. Relevant for Emacs
 # DescTools - for Winsorize function
-pkgs <- c("skimr", "codebook", "tcltk", "DescTools", "data.table", "haven")
+pkgs <- c("skimr", "codebook", "tcltk", "DescTools", "here", "data.table", "haven")
 invisible(lapply(pkgs, function(pkg) {
     if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
     library(pkg, character.only = TRUE)
@@ -13,6 +13,8 @@ invisible(lapply(pkgs, function(pkg) {
 Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
 
 setwd("O:\\Prosjekt\\Rusdata")
-DT <- haven::read_dta(file.path("Rusundersøkelsen", "Rusus 2024", "nytt forsøk februar 25 rus24.dta"))
+## DT <- haven::read_dta(file.path("Rusundersøkelsen", "Rusus 2024", "nytt forsøk februar 25 rus24.dta"))
+## saveRDS(DT, file.path("Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
+DT <- readRDS(file.path("Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
 dt <- as.data.table(DT)
 
