102d706c40c020ed57cee59f97d6573484c67cd7
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-07-31 13:09:32 +0200

    Specific to R packages only

--- rusund/setup.R
+++ rusund/setup.R
@@ -11,10 +11,3 @@ invisible(lapply(pkgs, function(pkg) {
 }))
 
 Sys.setlocale("LC_ALL", "nb-NO.UTF-8")
-
-setwd("O:\\Prosjekt\\Rusdata")
-## DT <- haven::read_dta(file.path("Rusundersøkelsen", "Rusus 2024", "nytt forsøk februar 25 rus24.dta"))
-## saveRDS(DT, file.path("Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
-DT <- readRDS(file.path("Rusundersøkelsen", "Rusus 2024","rus2024.rds"))
-dt <- as.data.table(DT)
-
