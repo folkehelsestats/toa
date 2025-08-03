35272498c27435cde0b1173fdb2fdf55403933d0
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-03 23:34:38 +0200

    Feat: Improve proscat function to handle char

--- rusund/functions.R
+++ rusund/functions.R
@@ -1,3 +1,5 @@
+source("~/Git-hdir/toa/rusund/fun-percent.R")
+
 ## var - variable for frequency
 ## dt - dataset to use
 ttbl <- function(var, d = dt, sum = TRUE, codebook = NULL, digits = 1){
@@ -32,44 +34,6 @@ get_freq_cat <- function(var, d = dt){
                         freq = bb[[1]]$freq$freq,
                         prosent = round(bb[[1]]$freq$perc*100, 1))
   data.table::setnames(freqTbl, "var", var)
-}
-
-## Percentage with 2 categories
-## proscat("Kjonn", "alko6")
-## total - Add total if TRUE
-proscat <- function(x, y, d = dt, na.rm = TRUE, digits = 1, total = TRUE){
-
-  elm <- any(class(d[[x]]) %in% c("numeric", "integer"))
-  if (isFALSE(elm))
-    stop(x, " is type: ", paste(class(d[[x]]), collapse = " "))
-
-  kb <- d[, .N, keyby = c(x, y)]
-
-  if (isTRUE(na.rm))
-    kb <- kb[!is.na(y), env = list(y = y)]
-
-  kbsum <- kb[, .(sum=sum(N)), by = c(x)]
-  kb[kbsum, on = x, sum := sum]
-
-  ## Total
-  val <- length(kbsum[[x]])+1 #need to know the max number of categories
-
-  velm <- is.element(val, kbsum[[x]])
-  if (isTRUE(velm))
-    stop(val, " can't be a total because it is part of the values in ", x)
-
-  if (total){
-    tot <- kb[, .(N=sum(N, na.rm = TRUE)), by = c(y)]
-    tot[, (x) := val]
-    tot[, sum := sum(N, na.rm = TRUE)]
-
-    DT <- data.table::rbindlist(list(kb, tot), use.names = TRUE)
-  } else {
-    DT <- kb
-  }
-
-  DT[, pros := round(N/sum*100, digits = digits)]
-  return(DT[])
 }
 
 
