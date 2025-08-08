7cfe89f2fa2d8c2703a45776856dbb6762e7c1ed
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-08 10:35:28 +0200

    Add common packages

--- rusund/setup.R
+++ rusund/setup.R
@@ -4,7 +4,10 @@
 # ----------------------------------
 # tcltk - to display the CRAN windows when not active. Relevant for Emacs
 # DescTools - for Winsorize function
-pkgs <- c("skimr", "codebook", "tcltk", "viridisLite", "DescTools", "gt", "here", "haven", "highcharter", "data.table")
+pkgs <- c("skimr", "codebook", "tcltk", "viridisLite",
+          "DescTools", "gt", "ggplot2", "here", "haven",
+          "rlang", "highcharter", "data.table")
+
 invisible(lapply(pkgs, function(pkg) {
     if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
     library(pkg, character.only = TRUE)
