40f95a2765aace0dd5ce951636da86cad1939427
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-13 20:57:44 +0200

    Include type1b 1 and 2

--- rusund/cleaning/history/merge-files.R
+++ rusund/cleaning/history/merge-files.R
@@ -69,19 +69,19 @@ varFelles <- Reduce(intersect, lapply(ComDD, tolower))
 ComVars <-
 c("helse", "drukket1", "drukket2", "drukk2a", "drukk2b", "drukk2c",
 "drukket3", "type1", "type2", "type3", "type4", "type1a", "typ1a_uk",
-"typ1a_mn", "type2a", "typ2a_uk", "typ2a_mn", "type2b_a", "type2b_b",
-"type2b_1", "type2b_2", "type2c_a", "type2c_b", "type2c_1", "type2c_2",
-"type3a", "typ3a_uk", "typ3a_mn", "type3b_a", "type3b_b", "type3b_1",
-"type3b_2", "type3c_a", "type3c_b", "type3c_1", "type3c_2", "type4a",
-"typ4a_uk", "typ4a_mn", "type4b_a", "type4b_b", "type4b_1", "type4b_2",
-"type4c_a", "type4c_b", "type4c_2", "audit2", "audit3", "can1",
-"can2", "can3", "can4", "can5", "can6", "can7_a", "can7_b", "can7_c",
-"can7_e", "can7sps", "can8", "can8sps", "can9", "can10", "can11",
-"can13", "can14", "ans1", "ans2_a", "ans2_b", "ans2_c", "ans2_d",
-"ans2_e", "ans2_f", "ans2_g", "ans2_h", "landsdel", "sentralitet",
-"yrkstat2", "siv", "sivstat", "antpers", "antbarn1", "antbarn2"
-)
-  
+"typ1a_mn", "type1b_1", "type1b_2", "type1c_1", "type2a", "typ2a_uk",
+"typ2a_mn", "type2b_a", "type2b_b", "type2b_1", "type2b_2", "type2c_a",
+"type2c_b", "type2c_1", "type2c_2", "type3a", "typ3a_uk", "typ3a_mn",
+"type3b_a", "type3b_b", "type3b_1", "type3b_2", "type3c_a", "type3c_b",
+"type3c_1", "type3c_2", "type4a", "typ4a_uk", "typ4a_mn", "type4b_a",
+"type4b_b", "type4b_1", "type4b_2", "type4c_a", "type4c_b", "type4c_2",
+"audit2", "audit3", "can1", "can2", "can3", "can4", "can5", "can6",
+"can7_a", "can7_b", "can7_c", "can7_e", "can7sps", "can8", "can8sps",
+"can9", "can10", "can11", "can13", "can14", "ans1", "ans2_a",
+"ans2_b", "ans2_c", "ans2_d", "ans2_e", "ans2_f", "ans2_g", "ans2_h",
+"landsdel", "sentralitet", "yrkstat2", "siv", "sivstat", "antpers",
+"antbarn1", "antbarn2")
+
 
 ## Find which vectors contain selected variable and show matches
 ## Alder --------------
