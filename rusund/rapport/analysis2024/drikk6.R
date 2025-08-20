4a98b32c6186e37fbdd4513af937f82eedca6802
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-20 15:27:50 +0200

    Use weighted percentage

--- rusund/rapport/analysis2024/drikk6.R
+++ rusund/rapport/analysis2024/drikk6.R
@@ -1,3 +1,5 @@
+## Andel drukket 6+ enheter siste år
+## ---------------------------------
 
 ## Nevner inkluderer de som ikke har drukket siste år også dvs. drukket1=2(Nei)
 dt[, drink6 := fcase(Audit3 %in% 1:4, 1, #Daglig, ukenlig, månedlig og sjeldenere
@@ -5,8 +7,8 @@ dt[, drink6 := fcase(Audit3 %in% 1:4, 1, #Daglig, ukenlig, månedlig og sjeldene
                      Drukket1 == 2, 0,
                      default = NA)]
 
-drk6_24 <- proscat(x = "kj", y = "drink6", d = dt, total = TRUE)
-kjonnkb <- data.table(v1 = 0:2, v2 = c("Menn", "Kvinner", "Totalt"))
+drk6_24 <- proscat_weighted(x = "kj", y = "drink6", d = dt, weight = vekt, total = TRUE)
+
 alkokb <- data.table(v1 = 0:1, v2 = c("Aldri", "Drukket 6+"))
 drk6_24[kjonnkb, on = c(kj = "v1"), gender := i.v2]
 drk6_24[alkokb, on = c(drink6 = "v1"), alko := i.v2]
