cf67840e77990b56edc121859c4f772e27def313
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-08-25 16:11:46 +0200

    Calculate units by alkohol type

--- rusund/dev/enheter.r
+++ rusund/dev/enheter.r
@@ -11,17 +11,21 @@ source(file.path(pth, "rus-draft.R"))
 alkohol_enhetskalkulator <- function(ren_alkohol_cl) {
   # Alkoholmengde per enhet
   ol_enhet <- 50 * 0.045     # 0,5 l øl med 4,5 %
-  vin_enhet <- 15 * 0.12     # 15 cl vin med 12 %
+  vin_enhet12 <- 12.5 * 0.12     # 12.5 cl vin med 12 %
+  vin_enhet15 <- 15 * 0.12     # 15 cl vin med 12 %
   sprit_enhet <- 4 * 0.40    # 4 cl sprit med 40 %
 
   # Beregning av antall enheter
   ol_antall <- ren_alkohol_cl / ol_enhet
-  vin_antall <- ren_alkohol_cl / vin_enhet
+  vin_antall12 <- ren_alkohol_cl / vin_enhet12
+  vin_antall15 <- ren_alkohol_cl / vin_enhet15
   sprit_antall <- ren_alkohol_cl / sprit_enhet
 
   # Resultattabell
   data.frame(
-    Drikke = c("Øl (0,5 l, 4,5%)", "Vin (15 cl, 12%)", "Sprit (4 cl, 40%)"),
-    Antall_enheter = round(c(ol_antall, vin_antall, sprit_antall), 2)
+    Drikke = c("Øl (50 cl, 4,5%)", "Vin (12.5 cl, 12%)", "Vin (15 cl, 12%)","Sprit (4 cl, 40%)"),
+    Antall_enheter = round(c(ol_antall, vin_antall12, vin_antall15, sprit_antall), 2)
   )
 }
+
+alkohol_enhetskalkulator(9)
