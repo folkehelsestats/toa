76dd51e507907adb3c743c62ae9f16b823b4dfd0
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-09-15 23:50:11 +0200

    Add emmeans package for marginal means adjusted

--- rusund/setup.R
+++ rusund/setup.R
@@ -6,7 +6,7 @@
 # DescTools - for Winsorize function
 pkgs <- c("skimr", "codebook", "tcltk", "viridisLite",
           "DescTools", "gt", "ggplot2", "here", "haven",
-          "rlang", "highcharter", "data.table")
+          "rlang","emmeans", "highcharter", "data.table")
 
 invisible(lapply(pkgs, function(pkg) {
     if (!requireNamespace(pkg, quietly = TRUE)) install.packages(pkg)
