c691d04c3c66d05d69c9d7de6bdf64fe265eaba2
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-09-15 11:43:40 +0200

    Rename all alkohol related variables

--- rusund/cleaning/history/merge-files.R
+++ rusund/cleaning/history/merge-files.R
@@ -72,6 +72,12 @@ beerVar2 <- c("type1c_1", "type1c_2")
 setnames(DD[["Rus2012"]], c("type1b_12012", "type1b_32012"), beerVar1, skip_absent = T)
 setnames(DD[["Rus2012"]], c("type1c_12012", "type1c_32012"), beerVar2, skip_absent = T)
 setnames(DD[["Rus2013"]], c("type1b_1", "type1b_3"), beerVar1, skip_absent = T)
+setnames(DD[["Rus2013"]], c("type1c_1", "type1c_3"), beerVar2, skip_absent = T)
+
+## DD[["Rus2012"]][, (beerVar1) := .SD, .SDcols = c("type1b_12012", "type1b_32012")]
+## DD[["Rus2012"]][, (beerVar2) := .SD, .SDcols = c("type1c_12012", "type1c_32012")]
+## DD[["Rus2013"]][, (beerVar1) := .SD, .SDcols = c("type1b_1", "type1b_3")]
+## DD[["Rus2013"]][, (beerVar2) := .SD, .SDcols = c("type1c_1", "type1c_3")]
 # Rus2020 har riktig kolonnavn, men de er tomme kolloner
 DD[["Rus2020"]][, (beerVar1) := .SD, .SDcols = c("type1b_11_a", "type1b_21_a")]
 DD[["Rus2020"]][, (beerVar2) := .SD, .SDcols = c("type1c_11_a", "type1c_21_a")]
@@ -96,8 +102,7 @@ rusbrusVar1 <- c("type4b_1", "type4b_2")
 rusbrusVar2 <- c("type4c_1", "type4c_2")
 DD[["Rus2020"]][, (rusbrusVar1) := .SD, .SDcols = c("type4b_11_a", "type4b_21_a")]
 DD[["Rus2020"]][, (rusbrusVar2) := .SD, .SDcols = c("type4c_11_a", "type4c_21_a")]
-
-
+DD[["Rus2021"]][, (rusbrusVar2[1]) := .SD, .SDcols = c("type4c_11")]
 
 
 ## Comman column names ------------------
@@ -110,22 +115,25 @@ for (i in seq_len(filnr)){
 names(nameDD) <- names(DD)
 
 varFelles <- Reduce(intersect, lapply(nameDD, tolower))
-## dput(varFelles)
-jComVars <-
+dput(varFelles)
+
+ComVars <-
 c("nyvekt2", "helse", "drukket1", "drukket2", "drukk2a", "drukk2b",
 "drukk2c", "drukket3", "type1", "type2", "type3", "type4", "type1a",
-"typ1a_uk", "typ1a_mn", "type1b_1", "type1b_2", "type1c_1", "type2a",
-"typ2a_uk", "typ2a_mn", "type2b_a", "type2b_b", "type2b_1", "type2b_2",
-"type2c_a", "type2c_b", "type2c_1", "type2c_2", "type3a", "typ3a_uk",
-"typ3a_mn", "type3b_a", "type3b_b", "type3b_1", "type3b_2", "type3c_a",
-"type3c_b", "type3c_1", "type3c_2", "type4a", "typ4a_uk", "typ4a_mn",
-"type4b_a", "type4b_b", "type4b_1", "type4b_2", "type4c_a", "type4c_b",
-"type4c_2", "audit2", "audit3", "can1", "can2", "can3", "can4",
-"can5", "can6", "can7_a", "can7_b", "can7_c", "can7_e", "can7sps",
-"can8", "can8sps", "can9", "can10", "can11", "can13", "can14",
-"ans1", "ans2_a", "ans2_b", "ans2_c", "ans2_d", "ans2_e", "ans2_f",
-"ans2_g", "ans2_h", "landsdel", "sentralitet", "yrkstat2", "siv",
-"sivstat", "antpers", "antbarn1", "antbarn2")
+"typ1a_uk", "typ1a_mn", "type1b_1", "type1b_2", "type1c_1", "type1c_2",
+"type2a", "typ2a_uk", "typ2a_mn", "type2b_a", "type2b_b", "type2b_1",
+"type2b_2", "type2c_a", "type2c_b", "type2c_1", "type2c_2", "type3a",
+"typ3a_uk", "typ3a_mn", "type3b_a", "type3b_b", "type3b_1", "type3b_2",
+"type3c_a", "type3c_b", "type3c_1", "type3c_2", "type4a", "typ4a_uk",
+"typ4a_mn", "type4b_a", "type4b_b", "type4b_1", "type4b_2", "type4c_a",
+"type4c_b", "type4c_1", "type4c_2", "audit2", "audit3", "can1",
+"can2", "can3", "can4", "can5", "can6", "can7_a", "can7_b", "can7_c",
+"can7_e", "can7sps", "can8", "can8sps", "can9", "can10", "can11",
+"can13", "can14", "ans1", "ans2_a", "ans2_b", "ans2_c", "ans2_d",
+"ans2_e", "ans2_f", "ans2_g", "ans2_h", "landsdel", "sentralitet",
+"yrkstat2", "siv", "sivstat", "antpers", "antbarn1", "antbarn2"
+)
+
 
 ## Find which vectors contain selected variable and show matches
 ## Alder --------------
